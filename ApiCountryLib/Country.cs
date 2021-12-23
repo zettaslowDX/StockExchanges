66fc67e20ff4e758ddef0d35d0e0c53ccf3a64e2
Franco Pereyra <francopereyra99@gmail.com>
2021-12-23 14:00:30 +0100

    Añadido de feature de countries Closes #3

---
+++ ApiCountryLib/Country.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiCountryLib
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Flags
+    {
+        public string svg { get; set; }
+        public string png { get; set; }
+    }
+
+    public class Currency
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class Language
+    {
+        public string iso639_1 { get; set; }
+        public string iso639_2 { get; set; }
+        public string name { get; set; }
+        public string nativeName { get; set; }
+    }
+
+    public class Translations
+    {
+        public string br { get; set; }
+        public string pt { get; set; }
+        public string nl { get; set; }
+        public string hr { get; set; }
+        public string fa { get; set; }
+        public string de { get; set; }
+        public string es { get; set; }
+        public string fr { get; set; }
+        public string ja { get; set; }
+        public string it { get; set; }
+        public string hu { get; set; }
+    }
+
+    public class RegionalBloc
+    {
+        public string acronym { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Country
+    {
+        public string name { get; set; }
+        public List<string> topLevelDomain { get; set; }
+        public string alpha2Code { get; set; }
+        public string alpha3Code { get; set; }
+        public List<string> callingCodes { get; set; }
+        public string capital { get; set; }
+        public List<string> altSpellings { get; set; }
+        public string subregion { get; set; }
+        public string region { get; set; }
+        public int population { get; set; }
+        public List<double> latlng { get; set; }
+        public string demonym { get; set; }
+        public double area { get; set; }
+        public List<string> timezones { get; set; }
+        public List<string> borders { get; set; }
+        public string nativeName { get; set; }
+        public string numericCode { get; set; }
+        public Flags flags { get; set; }
+        public List<Currency> currencies { get; set; }
+        public List<Language> languages { get; set; }
+        public Translations translations { get; set; }
+        public string flag { get; set; }
+        public List<RegionalBloc> regionalBlocs { get; set; }
+        public string cioc { get; set; }
+        public bool independent { get; set; }
+    }
+
+
+}
---
+++ BagMVC/Controllers/CountriesController.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BagLib;
+using BagLib.Models;
+
+namespace BagMVC.Controllers
+{
+    public class CountriesController : _BaseController
+    {
+  
+        public CountriesController(BagContext context) : base(context)
+        {
+        }
+
+        // GET: Countries
+        public async Task<IActionResult> Index()
+        {
+            return base.View(
+                await _context.GetCountriesAsync()
+                );
+        }
+
+
+
+        // GET: Countries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Country country = await _context.GetCountryAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+
+
+        //// GET: Countries/Create
+        //public IActionResult Create()
+        //{
+        //    List<Currency> currencies = new List<Currency>();
+
+        //    currencies.Add(new Currency((int)CurrencyType.Dolar, CurrencyType.Dolar.ToString()));
+        //    currencies.Add(new Currency((int)CurrencyType.Pound, CurrencyType.Pound.ToString()));
+        //    currencies.Add(new Currency((int)CurrencyType.Jen, CurrencyType.Jen.ToString()));
+
+        //    ViewData["Currencies"] = new SelectList(currencies, "Id", "Name");
+
+        //    return View();
+        //}
+
+        //// POST: Countries/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("CountryId,Name,CurrencyType")] Country country)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(country);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(country);
+        //}
+
+        // GET: Countries/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var country = await _context.Country.FindAsync(id);
+        //    if (country == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(country);
+        //}
+
+        //// POST: Countries/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("CountryId,Name,CurrencyType")] Country country)
+        //{
+        //    if (id != country.CountryId)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(country);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!CountryExists(country.CountryId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(country);
+        //}
+
+        // GET: Countries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Country
+                .FirstOrDefaultAsync(m => m.CountryId == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        // POST: Countries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var country = await _context.Country.FindAsync(id);
+            _context.Country.Remove(country);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update()
+        {
+            ApiCountryLib.ApiCountryClient myApi = new ApiCountryLib.ApiCountryClient("https://restcountries.com", 2);
+
+            var myResult = await myApi.GetCountries();
+
+            foreach ( var country in myResult )
+            {
+                // 1. Compruebo que exista país
+                var dbCountry = await _context.GetCountryByNameAsync(country.name);
+
+                // 1.2 Si no existe, inserto el país
+                if ( dbCountry==null )
+                {
+                    dbCountry = new Country(country.name);
+
+                    dbCountry= await _context.InsertCountryAsync(dbCountry);
+                }
+
+                if ( country.currencies!=null )
+                {
+                    // Para cada moneda
+                    foreach (var currency in country.currencies )
+                    {
+                        // 2. Compruebo si existe la moneda
+                        var dbCurrency = await _context.GetCurrencyAsync(currency.code);
+
+                        // 2.1 Si no existe, inserto la moneda
+                        if ( dbCurrency == null )
+                        {
+                            dbCurrency = new(currency.code, currency.name, currency.symbol);
+
+                            dbCurrency = await _context.InsertCurrencyAsync(dbCurrency);
+
+                            // Sólo añade moneda a un país cuándo inserta
+                            //// Si modifico las monedas relacionadas, guardo cambios
+                            //dbCountry.Currencies.Add(dbCurrency);
+
+                            //await _context.SaveChangesAsync();
+                        }
+
+                        // 3. Compruebo si el país de MI base de datos tiene esa moneda
+                        var currencyCountry = dbCountry.Currencies.FirstOrDefault(d => d.Code == dbCurrency.Code);
+
+                        // 3.1 Si no la tiene, la inserto
+                        if ( currencyCountry == null )
+                        {
+                            dbCountry.Currencies.Add(dbCurrency);
+
+                            await _context.SaveChangesAsync();
+                        }
+                    }
+                }
+                else
+                {
+                    // Países sin moneda
+                }
+
+                
+
+
+            }
+            
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CountryExists(int id)
+        {
+            return _context.Country.Any(e => e.CountryId == id);
+        }
+    }
+}
