84f9455e540adabc25952d94ab189e1a1ca8f1a6
Franco Pereyra <francopereyra99@gmail.com>
2021-12-23 13:53:44 +0100

    Añadido de feature de Stock Management Closes #1

---
+++ ApiStocksLib/ApiStockClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiStocksLib
+{
+    public class ApiStockClient
+    {
+        public ApiStockClient(string mUrl, string key)
+        {
+            this.mUrl = mUrl;
+            Key = key;
+        }
+
+        protected string mUrl { get; set; }
+
+        protected string Key { get; set; }
+
+        protected HttpClient client { get; set; }
+
+
+        protected string GetRelative(string apiFunction, string querystring)
+        {
+            //https://www.alphavantage.co/ query?function=SYMBOL_SEARCH&keywords=BA&apikey=demo
+
+            return $"query?function={apiFunction}&{querystring}&apikey={Key}";
+        }
+
+        public async Task<RootBestMatches> GetSearch(string text)
+        {
+            client = new HttpClient();
+
+            client.BaseAddress = new Uri(mUrl);
+
+            var response = await client.GetAsync(GetRelative("SYMBOL_SEARCH", $"keywords={text}"));
+
+            if ( response.IsSuccessStatusCode )
+            {
+                string s = await response.Content.ReadAsStringAsync();
+
+                return await response.Content.ReadAsAsync<RootBestMatches>();
+            }
+
+            return null;
+        }
+
+        public async Task<Company> GetCompany(string companyName)
+        {
+            client = new HttpClient();
+
+            client.BaseAddress = new Uri(mUrl);
+
+            var response = await client.GetAsync(GetRelative("OVERVIEW", $"symbol={companyName}"));
+
+            if (response.IsSuccessStatusCode)
+            {
+                //string s = await response.Content.ReadAsStringAsync();
+
+                return await response.Content.ReadAsAsync<Company>();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ApiStocksLib/BestMatch.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiStocksLib
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class BestMatch
+    {
+        [JsonProperty("1. symbol")]
+        public string Symbol { get; set; }
+
+        [Display(Name="Nombre")]
+        [JsonProperty("2. name")]
+        public string Name { get; set; }
+
+        [JsonProperty("3. type")]
+        public string Type { get; set; }
+
+        [JsonProperty("4. region")]
+        public string Region { get; set; }
+
+        [JsonProperty("5. marketOpen")]
+        public string MarketOpen { get; set; }
+
+        [JsonProperty("6. marketClose")]
+        public string MarketClose { get; set; }
+
+        [JsonProperty("7. timezone")]
+        public string Timezone { get; set; }
+
+        [JsonProperty("8. currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("9. matchScore")]
+        public string MatchScore { get; set; }
+    }
+
+    public class RootBestMatches
+    {
+        public List<BestMatch> bestMatches { get; set; }
+
+        [JsonProperty("Error Message")]
+        public string Error { get; set; }
+
+        public bool IsError => Error != null;
+    }
+}
